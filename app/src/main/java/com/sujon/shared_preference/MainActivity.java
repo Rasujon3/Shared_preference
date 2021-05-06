a661aaa2c7982ab81176ca881a43be90b157efde
Ruhul Amin Sujon <rasujon3@gmail.com>
2021-05-06 19:21:35 +0600

    1_Shared Preference - Android Development with Java - Rakibul Islam - Bohubrihi
    1_Shared Preference - Android Development with Java - Rakibul Islam - Bohubrihi

--- app/src/main/java/com/sujon/shared_preference/MainActivity.java
+++ app/src/main/java/com/sujon/shared_preference/MainActivity.java
@@ -1,5 +1,6 @@
 package com.sujon.shared_preference;
 
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import androidx.appcompat.app.AppCompatActivity;
@@ -19,6 +20,13 @@ public class MainActivity extends AppCompatActivity {
         userEmailText = findViewById(R.id.editTextTextEmailAddress);
         userPasswordText = findViewById(R.id.editTextTextPassword);
         submitButton = findViewById(R.id.button);
+
+        submitButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                
+            }
+        });
 
     }
 }
