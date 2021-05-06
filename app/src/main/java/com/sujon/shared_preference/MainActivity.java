e0f2e6d10f39f22a4b2f9b42c61c1913923394c5
Ruhul Amin Sujon <rasujon3@gmail.com>
2021-05-06 19:50:26 +0600

    2_Save a value - Android Development with Java - Rakibul Islam - Bohubrihi
    2_Save a value - Android Development with Java - Rakibul Islam - Bohubrihi

--- app/src/main/java/com/sujon/shared_preference/MainActivity.java
+++ app/src/main/java/com/sujon/shared_preference/MainActivity.java
@@ -1,8 +1,10 @@
 package com.sujon.shared_preference;
 
+import android.content.SharedPreferences;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -10,6 +12,8 @@ public class MainActivity extends AppCompatActivity {
 
     EditText userEmailText,userPasswordText;
     Button submitButton;
+    SharedPreferences preferences;
+    String myPreferenceName = "SHAREPREFERENCE_FILEONE";
 
 
     @Override
@@ -20,11 +24,30 @@ public class MainActivity extends AppCompatActivity {
         userEmailText = findViewById(R.id.editTextTextEmailAddress);
         userPasswordText = findViewById(R.id.editTextTextPassword);
         submitButton = findViewById(R.id.button);
+        preferences = getSharedPreferences(myPreferenceName,MODE_PRIVATE);
+
+        if (preferences.getBoolean("IS_LOGGEDIN",false)){
+            String email = preferences.getString("USER_EMAIL",null);
+            if (email!=null)
+                Toast.makeText(this,"WELCOME "+email,Toast.LENGTH_SHORT).show();
+
+        } else {
+            Toast.makeText(this,"Please Log in first",Toast.LENGTH_SHORT).show();
+        }
 
         submitButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                
+                String email = userEmailText.getText().toString();
+                String password = userPasswordText.getText().toString();
+
+                SharedPreferences.Editor editor = preferences.edit();
+                editor.putString("USER_EMAIL",email);
+                editor.putBoolean("IS_LOGGEDIN",true);
+
+//                editor.commit();
+                editor.apply();
+
             }
         });
 
