78cfc4891980d309f14b7a5551192e1d192d0569
Ruhul Amin Sujon <rasujon3@gmail.com>
2021-05-06 19:19:37 +0600

    Initial commit

---
+++ app/src/main/java/com/sujon/shared_preference/MainActivity.java
@@ -0,0 +1,24 @@
+package com.sujon.shared_preference;
+
+import android.widget.Button;
+import android.widget.EditText;
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    EditText userEmailText,userPasswordText;
+    Button submitButton;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        userEmailText = findViewById(R.id.editTextTextEmailAddress);
+        userPasswordText = findViewById(R.id.editTextTextPassword);
+        submitButton = findViewById(R.id.button);
+
+    }
+}
